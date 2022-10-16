a06bb40f92f39159c340f80653634befa4be96bc
pabrivjim <parijimenez@gmail.com>
2022-10-16 13:52:51 +0200

    document test

--- proteus/tests/test_directories.py
+++ proteus/tests/test_directories.py
@@ -22,13 +22,16 @@ import proteus
 from proteus.app import ProteusApplication
 from proteus.config import Config
 from proteus.model.archetype_manager import ArchetypeManager
-from proteus.model.archetype_proxys import ProjectArchetypeProxy
+from proteus.model.archetype_proxys import DocumentArchetypeProxy, ProjectArchetypeProxy
+from proteus.model.object import Object
 from proteus.model.project import Project
 
 # --------------------------------------------------------------------------
 # Tests
 # --------------------------------------------------------------------------
 app : Config = Config()
+test_project : Project = Project.load(app.base_directory / "tests" / "project")
+
 
 def test_application_directories():
     """
@@ -58,3 +61,44 @@ def test_project_archetype_manager():
         # Check we can get an instance of the project.
         project = project_arch.get_project()
         assert type(project) is Project
+
+        # Get each project folder and their files
+        project_dir = os.path.dirname(project_arch.path)
+        project_files = [file for file in os.listdir(project_dir)]
+
+        # Check if the project has assets and objects
+        assert  all(elem in project_files for elem in ["assets", "objects"])
+
+        # Check if the project has at least one .xml file (the project file)
+        assert len([x for x in project_files if x.endswith('.xml')]) >= 1
+            
+            
+def test_document_archetype_manager():
+
+    # Get the number of documents in archetypes documents
+    dir_path = str(app.archetypes_directory / "documents")
+    number_of_documents = len(os.listdir(dir_path))
+    
+    # Check if load document function return all the documents
+    documents = ArchetypeManager.load_document_archetypes()
+    assert len(documents) == number_of_documents
+
+    # Check if the document is a DocumentArchetypeProxy and has all the attributes
+    for document_arch in documents:
+        assert all(x for x in [type(document_arch) is DocumentArchetypeProxy, 
+                               document_arch.path, document_arch.id, document_arch.name, document_arch.description,
+                               document_arch.author, document_arch.date])
+        
+        # Check we can get an instance of the document.
+        document = document_arch.get_document(test_project)
+        assert type(document) is Object
+
+        # Get each document folder and their files
+        document_dir = os.path.dirname(os.path.dirname(document_arch.path))
+        document_files = [file for file in os.listdir(document_dir)]
+
+        # Check if the document has assets and objects
+        assert all(elem in document_files for elem in ["assets", "objects"])
+
+        # Check if the document archetype has a document.xml file
+        assert len([x for x in document_files if (x == "document.xml")]) == 1
